1c0adde3620341b37b0a7193ac6386dc273469fd
ct <852051047@qq.com>
2022-10-17 15:24:42 +0800

    10/17   15.25   更新代码

--- server.js
+++ server.js
@@ -1,98 +1,82 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
-const { unescape } = require("querystring");
-var port = process.argv[2];
 
+//获取输入的端口
+var port = process.argv[2];
 if (!port) {
-  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  console.log("请指定端口例如: node server.js 8888");
   process.exit(1);
 }
 
-var server = http.createServer(function (request, response) {
-  var parsedUrl = url.parse(request.url, true);
-  var pathWithQuery = request.url;
-  var queryString = "";
-  if (pathWithQuery.indexOf("?") >= 0) {
-    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"));
+// 读取文件目录
+fs.readdir('./public/img', (err, files) => {
+  if (err) {
+    return console.log('目录不存在')
   }
-  var path = parsedUrl.pathname;
-  var query = parsedUrl.query;
-  var method = request.method;
+  console.log(files);   //['功能.jpg','垃圾屋.jpg','投喂.jpg']
+  let htmlStr = ''         //根据文件名,拼接标签
+  for (let i = 0; i < files.length; i++) {
+    htmlStr += `
+    <details>
+        <summary>${files[i].split('.')[0]}</summary>
+        <img src="./img/${files[i]}" alt="" width="100%">
+    </details>
+  `
+  }
 
-  // ********获取目录里的所有文件名**********
-  const dir = './public';
-  let imageNameArr = []
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      throw err;
-    }
-    files.forEach(file => {
-      if (file !== 'index.html' && file !== 'bg.jpg') {
-        imageNameArr.push(file.split('.')[0])
-      }
-    });
-    console.log(imageNameArr);
-    // 根据图片数量, 追加标签
-    let htmlStr = ''
-    for (let i = 0; i < imageNameArr.length; i++) {
-      htmlStr += `
-      <details>
-          <summary>${imageNameArr[i]}</summary>
-          <img src="${imageNameArr[i]}.jpg" alt="" width="100%">
-      </details>
-    `
-    }
+  // 创建http服务+注册请求事件 (拿到请求,返回响应)
+  let server = http.createServer((request, response) => {
+    // console.log(request.headers);      //请求头
+    // console.log(request.method);       //请求方式
+    // console.log(request.url)           //"http:/域名:8888/xx.html?a=b"
 
-    /******** 从这里开始看，上面不要看 ************/
-    console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + decodeURI(pathWithQuery));
-    response.statusCode = 200;
-    //没路径请求主页, 有路径写路径(解码以防中文)
-    let filePath = path === "/" ? "/index.html" : decodeURI(path);
-    //文件后缀
-    let suffix = filePath.replace(/.*\./, "");
-    //后缀支持的类型
-    ContentTypes = {
+    // 地址==>对象, 顺便解码中文
+    var parsedUrl = url.parse(decodeURI(request.url), true);
+    // console.log(parsedUrl.href);       //"http:/域名:8888/xx.html?a=b"
+    // console.log(parsedUrl.path);       //"/xx.html?a=b"
+    // console.log(parsedUrl.pathname);   //"/xx.html"
+    // console.log(parsedUrl.query);      //{a:b}
+
+    // ----------------------------------------------------------------------
+    console.log('接收到请求地址: ' + parsedUrl.href);
+
+    //如果没有路径名, 则进入首页
+    if (parsedUrl.pathname === "/") {
+      parsedUrl.pathname = "/index.html"
+    }
+    //读取文件后缀 (html/css/js)
+    let suffix = parsedUrl.pathname.replace(/.*\./, "");
+    //支持的后缀类型
+    let ContentTypes = {
       html: "text/html",
       css: "text/css",
       js: "text/javascript",
       png: "image/png",
       jpg: "image/jpeg",
     };
-    //设置响应体格式 (没支持的用text/html兜底)
-    response.setHeader(
-      "Content-Type",
-      `${ContentTypes[suffix] || "text/html"};charset=utf-8`
-    );
+    //设置响应体格式 (没支持的后缀用text/html兜底)
+    response.setHeader("Content-Type", `${ContentTypes[suffix] || "text/html"};charset=utf-8`);
+
     //读取文件
     let content;
     try {
-      if (filePath === '/index.html') {
-        content = fs.readFileSync(`public${filePath}`).toString();
+      if (parsedUrl.pathname === '/index.html') {
+        content = fs.readFileSync(`public${parsedUrl.pathname}`).toString();
         content = content.replace('{{n}}', htmlStr)
       } else {
-        content = fs.readFileSync(`public${filePath}`);
+        content = fs.readFileSync(`public${parsedUrl.pathname}`);
       }
     } catch (error) {
-      content = "文件不存在";
       response.statusCode = 404;
+      content = "文件不存在";
     }
-
-    //写入响应体
-    response.write(content);
-    response.end();
-
-    /******** 代码结束，下面不要看 ************/
+    response.statusCode = 200;
+    //设置响应体, 并响应
+    response.end(content);
   })
 
-
-
-});
-
-server.listen(port);
-console.log(
-  "监听 " +
-  port +
-  " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
-  port
-);
+  //监听端口, 启动服务
+  server.listen(port)
+  console.log(`\n服务启动成功, 请打开http://localhost:${port}`);
+})
